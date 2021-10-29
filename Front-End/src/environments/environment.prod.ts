9ae99f461e67029a4c7e172b9cd4019e8d1c4fc1
Daniel Massoudyan <47738720+massoudyan1@users.noreply.github.com>
2021-10-29 23:11:04 +0200

    123

--- Front-End/src/environments/environment.prod.ts
+++ Front-End/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAgT-7yoLc9Z3gmmA0YNYJvy2md8ZJIKz4',
+    authDomain: 'the-tasty-bible.firebaseapp.com',
+    projectId: 'the-tasty-bible',
+    storageBucket: 'the-tasty-bible.appspot.com',
+    messagingSenderId: '668126688675',
+    appId: '1:668126688675:web:20acd74c6c8da558765616',
+    measurementId: 'G-MBKEKN8293',
+  },
 };
